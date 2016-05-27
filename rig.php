fed850ed5d5fddf28c36d5ec86d38ad5d6dcb8fc
James Schumann <jschumann@gmail.com>
2016-05-27 19:03:44 -0400

    Create rig.php

---
+++ rig.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* Plugin Name: WRDSB RIG
+* Plugin URI: https://github.com/wrdsb/wordpress-plugin-rig
+* Description: Runtime Intelligence Gathering for WordPress
+* Author: WRDSB
+* Author URI: https://github.com/wrdsb
+* Version: 0.0.1
+* License: GNU AGPLv3
+* GitHub Plugin URI: wrdsb/wordpress-plugin-rig
+* GitHub Branch: master
+*/
