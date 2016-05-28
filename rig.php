7e68d73efad90ca910f722629738f5b897279b6f
James Schumann <jschumann@gmail.com>
2016-05-27 20:25:21 -0400

    add total network post and page counts

--- rig.php
+++ rig.php
@@ -10,3 +10,38 @@
 * GitHub Plugin URI: wrdsb/wordpress-plugin-rig
 * GitHub Branch: master
 */
+
+add_action('network_admin_menu', 'wrdsb_rig_menu');
+
+function wrdsb_rig_menu() {
+	add_menu_page("Runtime Intelligence Gathering","RIG",'capability','wrdsb-rig','wrdsb_rig_page');
+}
+
+function wrdsb_rig_page() {
+	$total_posts = 0;
+	$total_pages = 0; 
+
+	$args = array(
+		'network_id' => null,
+		'public'     => null,
+		'archived'   => null,
+		'mature'     => null,
+		'spam'       => null,
+		'deleted'    => null,
+		'limit'      => 100,
+		'offset'     => 0,
+	);
+	$sites = wp_get_sites( $args );
+
+	foreach( $sites as $site ){
+		switch_to_blog( $site['blog_id'] );
+		$count_posts = wp_count_posts();
+		$total_posts += $count_posts->publish;
+		$count_pages = wp_count_posts('page');
+		$total_pages += $count_pages->publish;
+		restore_current_blog();
+	}
+
+	echo "<p><strong>Total Posts (published):</strong> " . $total_posts . "</p>";
+	echo "<p><strong>Total Pages (published):</strong> " . $total_pages . "</p>";
+}
