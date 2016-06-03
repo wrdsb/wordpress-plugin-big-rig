353692f60c068bd6638814656bf6a3ee9ba917f3
James Schumann <jschumann@gmail.com>
2016-06-03 14:05:54 -0400

    starting support for other post statuses

--- gauges/BigRigPostsGauge.php
+++ gauges/BigRigPostsGauge.php
@@ -57,4 +57,40 @@ public static function get_network_posts_published() {
 	return $total_posts;
 }
 
+public static function get_network_post_count_published() {
+	$post_counts = self::get_network_post_counts();
+	return $post_counts['publish'];
+}
+
+private static function get_network_post_counts() {
+	$post_counts = array(
+		'publish' => 0,
+		'future' => 0,
+		'draft' => 0,
+		'pending' => 0,
+		'private' => 0,
+		'trash' => 0,
+		'auto-draft' => 0,
+		'inherit' => 0
+	);
+	$args = array(
+		'network_id' => null,
+		'public'     => null,
+		'archived'   => null,
+		'mature'     => null,
+		'spam'       => null,
+		'deleted'    => null,
+		'limit'      => 4000,
+		'offset'     => 0,
+	);
+	$sites = wp_get_sites( $args );
+	foreach( $sites as $site ){
+		switch_to_blog( $site['blog_id'] );
+		$count_posts = wp_count_posts();
+		if (isset($count_posts->publish)) { $post_counts['publish'] += $count_posts->publish; }
+		restore_current_blog();
+	}
+	return $post_counts;
+}
+
 } // end class
